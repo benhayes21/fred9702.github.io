7538330a49b94ee95618c46c3defa8305f5729be
fred9702 <35751572+fred9702@users.noreply.github.com>
2018-08-21 14:45:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$.stellar();
+
+(function(){
+    var ua = navigator.userAgent,
+        isMobileWebKit = /WebKit/.test(ua) && /Mobile/.test(ua);
+    
+    if (isMobileWebKit) {
+        $(html).addClass('mobile');
+    }
+        
+    $(function(){
+        var iScrollInstance;
+        
+        if (isMobileWebKit) {
+            iScrollInstance = new iScroll('wrapper');
+            
+            $('#scroller').stellar({
+                scrollProperty: 'transform',
+                positionProperty: 'transform',
+                horizontalScrolling: false,
+                verticalOffset: 150
+            });
+        } else {
+            $.stellar({
+                horizontalScrolling: false,
+                verticalOffset: 150
+            });
+        }
+        
+    });
+    
+})();
